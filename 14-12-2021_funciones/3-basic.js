10c8c09f3ec596af52de7987c62d59756e1ae439
alexander febres <alexanderfebresth@gmail.com>
2021-12-14 09:18:13 -0500

    Adding new exercise

---
+++ 14-12-2021_funciones/3-basic.js
@@ -0,0 +1,10 @@
+// generar un valor random en un rango de 1 a 100 (incluyendo el 100)
+
+let min = 1
+let max = 101
+
+const firstRandom = Math.floor(Math.random() * max);
+const secondRandom = Math.floor(Math.random() * max)
+
+console.log(firstRandom)
+console.log(secondRandom)
